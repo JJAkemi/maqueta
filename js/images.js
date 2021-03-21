bd96c9779459edd8dfea998fddfdc9be4d4266bd
jonathanAkm <jjcg2005@gmail.com>
2021-03-21 01:18:42 -0500

    Add files via upload
    consumo de json para cargar el grid

--- js/images.js
+++ js/images.js
@@ -1,5 +1,51 @@
-var app={};
-var imgGrid=function(datos){
-    console.log(datos);
-    app.images=datos;
-}
+
+        var img1=imgGrid[0].url;
+        var desc1=imgGrid[0].desc;
+        var img2=imgGrid[1].url;
+        var desc2=imgGrid[1].desc;
+        var img3=imgGrid[2].url;
+        var desc3=imgGrid[2].desc;
+        var img4=imgGrid[3].url;
+        var desc4=imgGrid[3].desc;
+        var img5=imgGrid[4].url;
+        var desc5=imgGrid[4].desc;
+        var img6=imgGrid[5].url;
+        var desc6=imgGrid[5].desc;
+        var img7=imgGrid[6].url;
+        var desc7=imgGrid[6].desc;
+        var img8=imgGrid[7].url;
+        var desc8=imgGrid[7].desc;
+        var img9=imgGrid[8].url;
+        var desc9=imgGrid[8].desc;
+        var img10=imgGrid[9].url; 
+        var desc10=imgGrid[9].desc;
+
+        document.getElementById("img01ID").src=img1; 
+        document.getElementById("img01ID").alt=desc1; 
+
+        document.getElementById("img02ID").src=img2; 
+        document.getElementById("img02ID").alt=desc2;
+
+        document.getElementById("img03ID").src=img3; 
+        document.getElementById("img03ID").alt=desc3;
+
+        document.getElementById("img04ID").src=img4; 
+        document.getElementById("img04ID").alt=desc4;
+
+        document.getElementById("img05ID").src=img5; 
+        document.getElementById("img05ID").alt=desc5;
+
+        document.getElementById("img06ID").src=img6; 
+        document.getElementById("img06ID").alt=desc6;
+
+        document.getElementById("img07ID").src=img7; 
+        document.getElementById("img07ID").alt=desc7;
+
+        document.getElementById("img08ID").src=img8; 
+        document.getElementById("img08ID").alt=desc8;
+
+        document.getElementById("img09ID").src=img9; 
+        document.getElementById("img09ID").alt=desc9;
+
+        document.getElementById("img10ID").src=img10; 
+        document.getElementById("img10ID").alt=desc10;
